518c05939d9b373afd09f9416bd16f1c04cb32e8
Zimian Xu <zimian.xu@thoughtworks.com>
2021-02-24 11:59:36 +0800

    update: add print format for mod_two in RemainderDemo

--- java/common/src/main/java/com/xuzimian/java/learning/common/calculation/RemainderDemo.java
+++ java/common/src/main/java/com/xuzimian/java/learning/common/calculation/RemainderDemo.java
@@ -5,8 +5,8 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 public class RemainderDemo {
     @ParameterizedTest
-    @ValueSource(ints = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10})
+    @ValueSource(ints = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 23})
     void should_print_result_when_mod_two(int operand) {
-        System.out.println(operand % 2);
+        System.out.println(String.format("%d %% 2 = %d", operand, operand % 2));
     }
 }
